d5eb4107921cc6c4ea029dc2cf0b000617e35d81
jyolo <438608613@qq.com>
2018-03-20 17:34:19 +0800

    修改完成 linkselect 组件

--- components/linkselect.php
+++ components/linkselect.php
@@ -4,27 +4,24 @@ use CMaker\Component;
 use CMaker\Maker;
 
 
-
-
-//use CMaker\Maker;
-//Route::rule('component/:component_name', function ($component_name,Maker $maker){
-//    return $maker->requset($component_name);
-//});
-//{:CMaker('linkselect')
-//    ->label('所属分类|所属仓库|产品列表')
-//    ->helpinfo('联动选择')
-//    ->linkfield('cid|wid|gid')
-//    ->select([
-//        ['table' => 'goods_category','field' => 'id,pid,cat_name','istree' => true ],
-//        ['table' => 'goods as a' ,'join' => 'warehouse as b ,a.wid = b.id' ,'field' => 'b.name,a.wid' ,'group' => 'wid' ],
-//        ['table' => 'goods']
-//    ])
-//    ->render()}
 /**
- * 幻灯片组件
+ * 联动选择组件
  * User: jyolo
  * Date: 2017/7/6
  * Time: 17:08
+ *
+{:CMaker('linkselect')
+->label('所属分类|所属仓库|选择货品')
+->helpinfo('联动选择')
+->linkfield('cid|wid|gid')
+->serverUrl(url('goodsin/linkselect'))
+->value('2|2|5')
+->select([
+['param' => ['type' => 'goods_cat'] ,'showfield' => 'id,cat_name' ],
+['param' => ['type' => 'goods_warehouse'] ,'showfield' => 'wid,wname'],
+['param' => ['type' => 'goods'] ,'showfield' => 'id,name']
+])
+->render()}
  */
 class linkselect extends Component
 {
@@ -35,40 +32,17 @@ class linkselect extends Component
      */
     public static function attr(){
         return [
-            'label' => '',
+            'label' => '联动选择1|联动选择2',
             'helpinfo' => '',
-            'linkfield' => '',
+            'linkfield' => 'pid|id',
+            'value' => '',
+            'serverUrl' => '',
             'select' => [],
         ];
 
     }
 
-    public static function get_select_one_option($option){
-        $Db = new \think\Db(); //这里的DB 根据不同的框架 调用对应的 Db 类
-        $str = '';
-        if($option['istree'] == true){
-            $res = Component::get_tree_array($option);
-            $field = explode(',',$option['field']);
 
-            foreach($res as $k => $v){
-                $str .= '<option value="'.$k.'">'.$v.'</option>';
-            }
-
-        }else{
-            $res = $Db::name($option['table'])->field($option['field'])->select()->toArray();
-            $field = explode(',',$option['field']);
-            $value_field = $field[0];
-            $show_field = $field[1];
-            foreach($res as $k => $v){
-                $str .= '<option value="'.$v[$value_field].'">'.$v[$show_field].'</option>';
-            }
-        }
-
-
-
-
-        return $str;
-    }
 
     /*
      * 设置dom结构
@@ -77,26 +51,17 @@ class linkselect extends Component
     public static function dom(){
         $attr = self::$attr;
 
-
-        $select_1 = $attr['select'][0];
-
-
         $label = explode('|',$attr['label']);
         $linkfield = explode('|',$attr['linkfield']);
 
         $str = '';
         foreach($label as $k => $v){
-            if($k == 0){
-                $option = self::get_select_one_option($select_1);
-            }else{
-                $option = '';
-            }
+
             $str .= <<<EOT
     <label class="layui-form-label">{$v}</label>
     <div class="layui-input-inline">
         <select name="{$linkfield[$k]}" lay-verify="required" lay-filter="{$attr['id']}_{$k}">
             <option value="">请选择</option>
-            {$option}
         </select>
     </div>
 EOT;
@@ -104,7 +69,7 @@ EOT;
         }
 
         $dom = <<<EOT
-            <div class="layui-form-item" component-name="linkage" id="{$attr['id']}">
+            <div class="layui-form-item" component-name="linkselect" id="{$attr['id']}">
                     {$str}
                     <div class="layui-form-mid layui-word-aux">{$attr['helpinfo']}</div>
                 </div>
@@ -114,13 +79,6 @@ EOT;
     }
 
 
-    /**
-     * 组件依赖的js 插件
-     */
-    public static function relyOnJsPlugin($static_path){
-
-
-    }
 
     /*
      * 设置组件的 javascript 脚本
@@ -128,92 +86,84 @@ EOT;
      * */
     public static function script($attr){
         $js =<<<EOT
-        p(attr.set.id);
-;layui.use(['form'], function(){
+        p(attr);
+;layui.use(['form','jquery'], function(){
         var form = layui.form;
-        var next_select_option = all_data =[];
-
-        form.on('select('+ attr.set.id +'_0)', function(data){
-            p(attr.set.select[1]);
-            $.post('/component/linkselect',attr.set.select[1],function(msg){
-                p(msg);
-             //  var next_option = '';
-             //   $.each(msg,function(i,n){
-             //       next_option += '<option value="'+n.wid+'"> '+n.name+' </option>';
-            //    });
-            //    $('select[lay-filter=linkage_2]').find('option').slice(1).remove();
-            //    $('select[lay-filter=linkage_2]').append(next_option);
-            //    form.render('select');
-            });
+        var $ = layui.jquery;
+          
+        var the_last_select = $('#'+attr.uniqid_id).find('select').length - 1;
+        var selects = $('#'+attr.uniqid_id).find('select');
+        var allparam = {};
+        var value = attr.set.value.split('|');
+        
+        selects.each(function(i,n){
+            if(i == 0){
+                if(!attr.set.select[i]) return;
+                $.post(attr.set.serverUrl,attr.set.select[i].param,function(msg){
+                    builder_options(n,msg,attr.set.select[i].showfield ,value[i]);
+                });
+            }
+            
+            if(i != the_last_select){ //最后一个slelect 无需监听选择事件
+                var filter = attr.set.id + '_'+ i; 
+                form.on('select('+ filter +')', function(data){
+                   if(i == 0){
+                        //第一个选择之后，清空后面的
+                        $.each(selects.slice(1) ,function(i,n){
+                            $(n).find('option').slice(1).remove();
+                        })
+                   }
+                
+                    var param = attr.set.select[i+1].param;
+                    param[data.elem.name] = data.value;
+                    allparam[data.elem.name] = data.value
+                    var marge_param = $.extend(param,allparam);
+                    
+                    $.post(attr.set.serverUrl,marge_param,function(msg){
+                        var showfield = attr.set.select[i+1].showfield;
+                        builder_options(selects.eq(i+1),msg,showfield ,value[i+1]);
+                    });
+                });
+            }
             
         });
-        form.on('select('+ attr.set.id +'_1)', function(data){
-            //p(attr.set);
-            //$.post('/component/linkselect',{
-            //    'where[cid]': data.value,
-            //    'group':'wid',
-            //    'field':'wid,b.name'
-           // },function(msg){
-            //    p(msg);
-                //var next_option = '';
-                //$.each(msg,function(i,n){
-                //    next_option += '<option value="'+n.wid+'"> '+n.name+' </option>';
-                //});
-                //$('select[lay-filter=linkage_3]').find('option').slice(1).remove();
-                //$('select[lay-filter=linkage_3]').append(next_option);
-                //form.render('select');
-            //});
-
-        });
+        
+        function builder_options(select_dom ,json , showfield ,value = false){
+            
+            $(select_dom).find('option').slice(1).remove();
+            var field = showfield.split(',');
+            
+            var option = '';
+            $.each(json,function(i,n){
+                if(value == n[field[0]]){
+                    var selected = 'selected';
+                }else{
+                    var selected = '';
+                }
+                option += '<option value="'+ n[field[0]] +'" '+selected+'>'+ n[field[1]] +'</option>';
+            })
+            
+            $(select_dom).append(option);
+            form.render('select');
+            
+            $(select_dom).next('.layui-unselect').find('dd').each(function(i,n){
+                  if($(n).attr('lay-value') == value){
+                    $(n).trigger('click');
+                  }
+            });
+            
+            
+            
+        }
+        
+        
+        
         
     })
 EOT;
         return $js;
     }
-    /*
-     * 处理路由来的请求
-     * @return string
-     * */
-    public static function requset(){
-        $db = new \think\Db(); //这里的DB 根据不同的框架 调用对应的 Db 类
-        $model = $db::name($_POST['table']);
-        p($_POST);
-        $allow_method = ['join','group','limit','field'];
 
-        foreach($_POST as $k => $v){
-            if(!in_array($k ,$allow_method))continue;
-            if($k == 'join'){
-                $v = explode(',',$v);
-                $model->$k($v[0],$v[1]);
-            }else{
-                $model->$k($v);
-            }
-
-
-        }
-        $res = $model->select();
-        p($res);
-        die();
-//        $post = $_POST['where'];
-//        $where = '';
-//        $model = $db::name('goods')->alias('a')->join('warehouse b','a.wid = b.id');
-//        if(isset($_POST['where'])){
-//            $where = _parseWhere($_POST['where']);
-//            $model->where($where);
-//        }
-//
-//        if(isset($_POST['group'])){
-//            $model->group( $_POST['group']);
-//        }
-//        if(isset($_POST['field'])){
-//            $model->field($_POST['field']);
-//        }
-//
-//        $res = $model->select();
-//
-//        return json($res); // 此 json函数 来至 tp ,根据不同的框架 调用对应的函数
-
-    }
 
     
 
